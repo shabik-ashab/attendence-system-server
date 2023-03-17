a6af8af5d1fee25a673a8f72d99415825c5f82f2
nub69 <shabik.ashab@gmail.com>
2023-03-17 12:07:49 +0600

    userSchema added

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  roles: [String],
+  accountStatus: String,
+});
+
+model("User", userSchema);
+
+const User = model("User", userSchema);
+module.exports = User;
